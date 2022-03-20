ae85ea4bfeeddf4d3ad72c14cc3f3553abcf45ac
Eliziane3011 <eliziane.oliveira.p@hotmail.com>
2022-03-20 20:54:32 -0300

    Commit Dados variaveis e operadores

---
+++ VariaveisDados/VariaveisDadosExercitando.java
@@ -0,0 +1,40 @@
+package VariaveisDados;
+
+public class VariaveisDadosExercitando {
+    public static void main(String[] args){
+        int i;
+        //int i; variáveis não devem ter o mesmo nome;
+        //int 1b; não deve começar com número;
+        int I;
+
+        i = 5;
+        I = 15;
+
+        final int j = 10;
+        // j = 15 valor constante, não muda; não utilizar $ nem _ e %;
+
+        int quantidadeProduto = 50;
+        final int NUMERO_TENTATIVAS = 5;
+        int QUANTIDADE_OPCOES = 25;
+
+
+        System.out.println(i);
+        System.out.println(I);
+
+        System.out.println(j);
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
